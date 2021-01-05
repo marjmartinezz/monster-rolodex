03f33f66b7bdcc44be5b38280d951ee7514f7a1f
Marjorie Martinez <marjorie.martinez@phitopolis.com>
2021-01-05 19:26:04 +0800

    Updates

---
+++ precache-manifest.2e6fe36a991f1a827ff60af7cb4ca241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc9a5e9160402d7e4ae361a6c332ad93",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "633b8cf5a576bc907096",
+    "url": "/monsters-rolodex/static/css/main.30f04434.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "633b8cf5a576bc907096",
+    "url": "/monsters-rolodex/static/js/main.91eb676b.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
